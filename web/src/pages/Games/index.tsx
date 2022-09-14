900b1e464b2290b91f3f2f8c2913d4a3dfcff95b
Arthur Lage <arthurlage2006@gmail.com>
2022-09-13 23:02:30 -0300

    mobile home page and web home page

--- web/src/pages/Games/index.tsx
+++ web/src/pages/Games/index.tsx
@@ -1,38 +1,116 @@
 import React from "react";
 
-import Logo from "../../assets/logo.svg";
-import MagnifierPlus from "../../assets/magnifier-plus.svg";
+import Logo from "/logo.svg";
+import { MagnifyingGlassPlus } from "phosphor-react";
+
+import Game from "/image 1.png";
 
 export function Games() {
   return (
     <div className="min-h-[100vh] flex flex-col justify-center items-center">
-      <img className="mb-20" src={Logo} alt="NLW e-sports Logo" />
+      <img className="mb-20 mt-20" src={Logo} alt="NLW e-sports Logo" />
 
-      <h1 className="mb-16 text-center text-white text-[4rem] font-black inline-block">
+      <h1 className="font-inter mb-16 text-center text-white text-[4rem] font-black inline-block">
         Seu{" "}
-        <span className="bg-clip-text bg-duo-text text-fill-transparent">
+        <span className="bg-clip-text bg-purple-green-yellow-gradient text-transparent">
           duo
         </span>{" "}
         está aqui.
       </h1>
 
-      <section></section>
+      <section className="mb-8 grid grid-cols-6 gap-6">
+        <a
+          className="rounded-lg relative hover:brightness-110 hover:scale-105 transition-all duration-150"
+          href="#"
+        >
+          <img src={Game} alt="" />
+          <div className="h-[7.5rem] justify-end bg-fade-black rounded-lg z-10 flex flex-col absolute bottom-0 left-0 px-4 pb-4 w-full">
+            <p className="font-bold font-inter text-base text-white">
+              League of Legends
+            </p>
+            <p className="font-inter mt-1 text-sm text-zinc-300">4 anúncios</p>
+          </div>
+        </a>
+        <a
+          className="rounded-lg relative hover:brightness-110 hover:scale-105 transition-all duration-150"
+          href="#"
+        >
+          <img src={Game} alt="" />
+          <div className="h-[7.5rem] justify-end bg-fade-black rounded-lg z-10 flex flex-col absolute bottom-0 left-0 px-4 pb-4 w-full">
+            <p className="font-bold font-inter text-base text-white">
+              League of Legends
+            </p>
+            <p className="font-inter mt-1 text-sm text-zinc-300">4 anúncios</p>
+          </div>
+        </a>
+        <a
+          className="rounded-lg relative hover:brightness-110 hover:scale-105 transition-all duration-150"
+          href="#"
+        >
+          <img src={Game} alt="" />
+          <div className="h-[7.5rem] justify-end bg-fade-black rounded-lg z-10 flex flex-col absolute bottom-0 left-0 px-4 pb-4 w-full">
+            <p className="font-bold font-inter text-base text-white">
+              League of Legends
+            </p>
+            <p className="font-inter mt-1 text-sm text-zinc-300">4 anúncios</p>
+          </div>
+        </a>
+        <a
+          className="rounded-lg relative hover:brightness-110 hover:scale-105 transition-all duration-150"
+          href="#"
+        >
+          <img src={Game} alt="" />
+          <div className="h-[7.5rem] justify-end bg-fade-black rounded-lg z-10 flex flex-col absolute bottom-0 left-0 px-4 pb-4 w-full">
+            <p className="font-bold font-inter text-base text-white">
+              League of Legends
+            </p>
+            <p className="font-inter mt-1 text-sm text-zinc-300">4 anúncios</p>
+          </div>
+        </a>
+        <a
+          className="rounded-lg relative hover:brightness-110 hover:scale-105 transition-all duration-150"
+          href="#"
+        >
+          <img src={Game} alt="" />
+          <div className="h-[7.5rem] justify-end bg-fade-black rounded-lg z-10 flex flex-col absolute bottom-0 left-0 px-4 pb-4 w-full">
+            <p className="font-bold font-inter text-base text-white">
+              League of Legends
+            </p>
+            <p className="font-inter mt-1 text-sm text-zinc-300">4 anúncios</p>
+          </div>
+        </a>
+        <a
+          className="rounded-lg relative hover:brightness-110 hover:scale-105 transition-all duration-150"
+          href="#"
+        >
+          <img src={Game} alt="" />
+          <div className="h-[7.5rem] justify-end bg-fade-black rounded-lg z-10 flex flex-col absolute bottom-0 left-0 px-4 pb-4 w-full">
+            <p className="font-bold font-inter text-base text-white">
+              League of Legends
+            </p>
+            <p className="font-inter mt-1 text-sm text-zinc-300">4 anúncios</p>
+          </div>
+        </a>
+      </section>
 
-      <div className="bg-[#2A2634] border-t-4 border-inherit rounded-lg py-6 px-8 flex items-center justify-between w-[1200px]">
-        <div className="flex flex-col">
-          <h2 className="text-white font-black text-2xl">
-            Não encontrou seu duo?
-          </h2>
-          <p className="text-zinc-400 text-base">
-            Publique um anúncio para encontrar novos players!
-          </p>
+      <div className="pt-[4px] mb-[8.25rem] rounded-lg bg-purple-green-yellow-gradient overflow-hidden">
+        <div className="bg-[#2A2634] rounded-lg py-6 px-8 flex items-center justify-between w-[75rem]">
+          <div className="flex flex-col">
+            <h2 className="text-white font-black text-2xl">
+              Não encontrou seu duo?
+            </h2>
+            <p className="font-inter text-zinc-400 text-base">
+              Publique um anúncio para encontrar novos players!
+            </p>
+          </div>
+          <button className="gap-3  flex items-center px-4 py-3 rounded-md bg-violet-500 transition-all duration-150 hover:bg-violet-600">
+            <MagnifyingGlassPlus size={24} color="#fff"/>
+
+            <span className="font-inter text-white font-medium text-base">
+              Publicar anúncio
+            </span>
+          </button>
         </div>
-        <button className="gap-3  flex items-center px-4 py-3 rounded-md bg-violet-500">
-          <img src={MagnifierPlus} alt="New adnouncement" />
-          <span className="text-white font-medium text-base">
-            Publicar anúncio
-          </span>
-        </button>
       </div>
     </div>
   );
--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -1,12 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.tsx"],
+  content: ["./src/**/*.tsx", "./index.html"],
   theme: {
     extend: {
       backgroundImage: {
-        "duo-text": "linear-gradient(to right, #9572FC, #43E7AD, #E1D55D)"
-      }
+        "purple-green-yellow-gradient":
+          "linear-gradient(to right, #9572FC, #43E7AD, #E1D55D)",
+        galaxy: "url(/galaxy.png)",
+        "fade-black": "linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 67.08%);"
+      },
+      fontFamily: {
+        inter: ["Inter", "monospace"]
+      },
     },
   },
   plugins: [],
-}
+};
